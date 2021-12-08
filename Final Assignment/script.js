6aa73cedfc466772a10943ddc8636c98928e9aff
dixith0599 <94888550+dixith0599@users.noreply.github.com>
2021-12-08 17:14:28 +0530

    Delete Final Assignment directory

--- Final Assignment/script.js
+++
@@ -1,9 +0,0 @@
-var a = {};
-function addnewarticle()
-{
-    var x=document.getElementById("username").value;
-    var y =document.getElementById("article").value;
-    a[x]=y;
-    localStorage.setItem('user', JSON.stringify(a));
-    console.log('test')
-}
